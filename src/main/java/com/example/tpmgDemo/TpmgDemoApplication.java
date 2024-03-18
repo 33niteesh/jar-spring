0c5daa1fd6afef9615322b9515e7b269a07599bf
SATYAPU NITEESH KUMAR <82573249+33niteesh@users.noreply.github.com>
2024-03-18 14:49:17 +0530

    Add files via upload

---
+++ src/main/java/com/example/tpmgDemo/TpmgDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tpmgDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpmgDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpmgDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tpmgDemo/TpmgDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tpmgDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpmgDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
